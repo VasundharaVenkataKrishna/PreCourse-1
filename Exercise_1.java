47c009413e32375738c1975c7b080b11ab75ad40
VasundharaVenkataKrishna <vasundharakrishna10@gmail.com>
2024-10-14 22:13:35 -0500

    Update_Exercise_1.java

--- Exercise_1.java
+++ Exercise_1.java
@@ -7,29 +7,54 @@ class Stack {
   
     boolean isEmpty() 
     { 
-        //Write your code here 
+	System.out.println("Top value is "+ top);
+	return (top < 0);
     } 
 
     Stack() 
     { 
-        //Initialize your constructor 
+        top = -1; 
     } 
   
     boolean push(int x) 
     { 
         //Check for stack Overflow
         //Write your code here
+	if (top >= MAX-1) {
+	    System.out.println("Out of Capacity");
+	    return false;
+	} else {
+	    top++;
+	    a[top] = x;
+	    System.out.println(x + " Is pushed to Stack");
+	    return true;
+	}
     } 
   
     int pop() 
     { 
         //If empty return 0 and print " Stack Underflow"
         //Write your code here
+	if (isEmpty()) {
+	    System.out.println("Stack underflow");
+	    return Integer.MIN_VALUE;
+	} else {
+	    int poppedElement = a[top];
+	    top--;
+	    return poppedElement;
+	}
     } 
   
     int peek() 
     { 
         //Write your code here
+	if (isEmpty()) {
+	    System.out.println("Stanck is empty");
+	    return Integer.MIN_VALUE;
+	} else {
+	    return a[top];
+	}
+	
     } 
 } 
   
@@ -41,6 +66,9 @@ class Main {
         s.push(10); 
         s.push(20); 
         s.push(30); 
-        System.out.println(s.pop() + " Popped from stack"); 
+        System.out.println(s.pop() + " Popped from stack");
+	System.out.println(s.pop() + " Popped from stack");
+	System.out.println(s.pop() + " Popped from stack");
+	System.out.println(s.pop() + " Popped from stack"); 
     } 
 }
